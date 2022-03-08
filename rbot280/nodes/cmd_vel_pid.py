c08d81b632f26cde2a2d01d106d61148f114ae6d
mikedef <mikedef@mit.edu>
2022-03-08 13:21:40 -0500

    pid controller cont.

--- rbot280/nodes/cmd_vel_pid.py
+++ rbot280/nodes/cmd_vel_pid.py
@@ -40,6 +40,8 @@ class Cmd_Vel_PID(object):
         r = rospy.Rate(2)
         # Subscribers
         rospy.Subscriber("cmd_vel", Twist, callback=self.cmd_vel_cb, queue_size=10)
+        # Start with global planner but try to transition to local path planner
+        rospy.Subscriber("move_base_node/GlobalPlanner/plan", Plan, callback=goal_cb, queue_size=10)
 
         # Publishers
         cmd_vel_pid_pub = rospy.Publisher("cmd_vel/pid", Twist, queue_size=10)
@@ -49,9 +51,12 @@ class Cmd_Vel_PID(object):
 
         # init pid vars
         # linear
+        self.error_x = 0.0
+        # setpoint desired_velocity
 
         # angular
-
+        self.error_yaw = 0.0
+        self.desired_yaw = 0.0  # Setpoint
 
         while not rospy.is_shutdown():
 
@@ -61,8 +66,42 @@ class Cmd_Vel_PID(object):
             cmd_vel_pid_pub.publish(cmd_vel_pid_msg)
             r.sleep()
 
-    def pid_linear(self):
-        None
+    def pid_linear(self, msg):
+        """ Linear PID """
+        return 1.0
 
-    def pid_angular(self):
-        None
+    def pid_angular(self, msg):
+        """ Angular PID """
+        return 0.0
+
+    def dynamic_callback(self, config, level):
+        rospy.loginfo("""Reconfigure Request: \
+        {linear_kp}, {linear_ki}, {linear_kp}, \
+        {angular_kp}, {angular_ki}, {angular_kp}, \
+        {desired_velocity} """.format(**config))
+
+        self.linear_kp = config['linear_kp']
+        self.linear_ki = config['linear_ki']
+        self.linear_kd = config['linear_kd']
+        self.angular_kp = config['angular_kp']
+        self.angular_ki = config['angular_ki']
+        self.angular_kd = config['angular_kd']
+
+        self.desired_velocity = config['desired_velocity']
+
+        return config
+
+    def cmd_vel_cb(self, msg):
+        """ cmd_vel_cb from the subscriber to get pose data """
+        self.linear_x = msg.linear.x  # Forward velocity (m/s)
+        self.angular_z = msg.angular.z  # Rotational velocity (rad/s)
+
+    def goal_cb(self, msg):
+        rospy.loginfo(msg.poses[0].pose)  # Figure out what is being published here and use the first as the first pose as the goal
+
+    if __name__ == '__main__':
+        try:
+            cmd_vel_pid = Cmd_Vel_PID()
+
+        except KeyboardInterrupt, rospyROSInterruptException:
+            pass
